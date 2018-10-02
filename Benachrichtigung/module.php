134a7e92e0fb0784ba2c9e0d86e60a2de3fd1422
Niels Thole <niels.thole@gmail.com>
2018-10-02 13:07:47 +0200

    Initial commit for the notification module

---
+++ Benachrichtigung/module.php
@@ -0,0 +1,271 @@
+<?
+
+    class Benachrichtigung extends IPSModule {
+
+        const SCRIPT_ACTION = 0;
+        const PUSH_NOTIFICATION_ACTION = 1;
+
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
+
+            $this->RegisterPropertyInteger("InputTriggerID", 0);
+            $this->RegisterPropertyString("NotificationLevels", "[]");
+            $this->RegisterVariableInteger("NotificationLevel", "Notification Level", "");
+            $this->RegisterScript("ResetScript", "Reset", "<? BN_Reset(IPS_GetParent(\$_IPS['SELF']));");
+            $this->RegisterTimer("IncreaseTimer", 0, 'BN_IncreaseLevel($_IPS[\'TARGET\']);');
+        }
+
+        public function ApplyChanges() {
+            //Never delete this line!
+            parent::ApplyChanges();
+
+            $triggerID = $this->ReadPropertyInteger("InputTriggerID");
+
+            $this->RegisterMessage($triggerID, 10603 /* VM_UPDATE */);
+        }
+
+        public function MessageSink ($TimeStamp, $SenderID, $Message, $Data) {
+            $triggerID = $this->ReadPropertyInteger("InputTriggerID");
+            if (($SenderID == $triggerID) && ($Message == 10603) && (boolval($Data[0])) && (GetValue($this->GetIDForIdent('NotificationLevel')) == 0)) {
+                $this->SetNotifyLevel(1);
+            }
+        }
+
+        public function GetConfigurationForm() {
+            $notificationValues = [];
+            $levelTable = json_decode($this->ReadPropertyString('NotificationLevels'), true);
+            for ($i = 1; $i <= sizeof($levelTable); $i++) {
+                $actionValues = [];
+                foreach ($levelTable[$i - 1]['actions'] as $action) {
+                    $actionValues[] = [ 'status' => $this->GetActionStatus($action) ];
+                }
+                $notificationValues[] = [
+                    'level' => strval($i),
+                    'status' => $this->GetLevelStatus($i),
+                    'actions' => $actionValues
+                ];
+            }
+
+            $form = [
+                'elements' => [
+                    [
+                        'type' => 'SelectVariable',
+                        'name' => 'InputTriggerID',
+                        'caption' => 'Trigger'
+                    ],
+                    [
+                        'type' => 'List',
+                        'name' => 'NotificationLevels',
+                        'caption' => 'Notification Levels',
+                        'rowCount' => 5,
+                        'add' => true,
+                        'delete' => true,
+                        'columns' => [
+                            [
+                                'name' => 'level',
+                                'caption' => 'Level',
+                                'width' => '75px',
+                                'add' => ''
+                            ],
+                            [
+                                'name' => 'duration',
+                                'caption' => 'Duration',
+                                'width' => '100px',
+                                'add' => 60,
+                                'edit' => [
+                                    'type' => 'NumberSpinner',
+                                    'suffix' => ' seconds'
+                                ]
+                            ],
+                            [
+                                'name' => 'actions',
+                                'caption' => 'Actions',
+                                'width' => '100px',
+                                'add' => [],
+                                'edit' => [
+                                    'type' => 'List',
+                                    'rowCount' => 5,
+                                    'add' => true,
+                                    'delete' => true,
+                                    'columns' => [
+                                        [
+                                            'name' => 'actionType',
+                                            'label' => 'Action',
+                                            'width' => '150px',
+                                            'add' => 0,
+                                            'edit' => [
+                                                'type' => 'Select',
+                                                'options' => [
+                                                    [
+                                                        'caption' => 'Script',
+                                                        'value' => self::SCRIPT_ACTION
+                                                    ],
+                                                    [
+                                                        'caption' => 'Push',
+                                                        'value' => self::PUSH_NOTIFICATION_ACTION
+                                                    ]
+                                                ]
+                                            ]
+                                        ],
+                                        [
+                                            'name' => 'recipient',
+                                            'label' => 'Recipient',
+                                            'width' => '150px',
+                                            'add' => '',
+                                            'edit' => [
+                                                'type' => 'ValidationTextBox'
+                                            ]
+                                        ],
+                                        [
+                                            'name' => 'title',
+                                            'label' => 'Title',
+                                            'width' => '100px',
+                                            'add' => '',
+                                            'edit' => [
+                                                'type' => 'ValidationTextBox'
+                                            ]
+                                        ],
+                                        [
+                                            'name' => 'message',
+                                            'label' => 'Message',
+                                            'width' => '200px',
+                                            'add' => '',
+                                            'edit' => [
+                                                'type' => 'ValidationTextBox'
+                                            ]
+                                        ],
+                                        [
+                                            'name' => 'messageVariable',
+                                            'label' => 'Message Variable',
+                                            'width' => '200px',
+                                            'add' => 0,
+                                            'edit' => [
+                                                'type' => 'SelectVariable'
+                                            ]
+                                        ]/*, // TODO: How to show status for actions?
+                                        [
+                                            'name' => 'status',
+                                            'label' => 'Status',
+                                            'width' => '70px',
+                                            'add' => ''
+                                        ]*/
+                                    ]
+                                ]
+                            ],
+                            [
+                                'name' => 'status',
+                                'label' => 'Status',
+                                'width' => '70px',
+                                'add' => ''
+                            ]
+                        ],
+                        'values' => $notificationValues
+                    ]
+                ]
+            ];
+
+            return json_encode($form);
+        }
+
+        public function SetNotifyLevel(int $level) {
+            SetValue($this->GetIDForIdent('NotificationLevel'), $level);
+
+            $levelTable = json_decode($this->ReadPropertyString('NotificationLevels'), true);
+
+            if ($level <= sizeof($levelTable)) {
+
+                foreach ($levelTable[$level - 1]['actions'] as $action) {
+                    $message = $action['message'];
+                    if ($action['messageVariable'] !== 0) {
+                        if ($message !== '') {
+                            $message .= "\n";
+                        }
+                        $message .= GetValue($action['messageVariable']);
+                    }
+                    switch ($action['actionType']) {
+                        case self::SCRIPT_ACTION:
+                            IPS_RunScript(intval($action['recipient']));
+                            break;
+
+                        case self::PUSH_NOTIFICATION_ACTION:
+                            WFC_PushNotification(intval($action['recipient']), $action['title'], $message, 'alarm', $this->GetIDForIdent('ResetScript')); // TODO: Jump to comfirm script
+                            break;
+                    }
+                }
+
+                if ($level < sizeof($levelTable)) {
+                    $this->SetTimerInterval('IncreaseTimer', $levelTable[$level - 1]['duration'] * 1000);
+                }
+                else {
+                    $this->SetTimerInterval('IncreaseTimer', 0);
+                }
+            }
+            else {
+                // TODO: Error message
+            }
+        }
+
+        public function IncreaseLevel() {
+            $this->SetNotifyLevel(GetValue($this->GetIDForIdent('NotificationLevel')) + 1);
+        }
+
+        public function Reset() {
+            $this->SetTimerInterval('IncreaseTimer', 0);
+            SetValue($this->GetIDForIdent('NotificationLevel'), 0);
+        }
+
+        private function GetActionStatus($actionObject) {
+            switch ($actionObject['actionType']) {
+                case self::SCRIPT_ACTION:
+                    if (!is_numeric($actionObject['recipient']) || (intval($actionObject['recipient']) < 10000) || (intval($actionObject['recipient']) > 59999)) {
+                        return $this->Translate('Invalid ID');
+                    }
+
+                    if (!IPS_ScriptExists(intval($actionObject['recipient']))) {
+                        return $this->Translate('No script');
+                    }
+                    break;
+
+                case self::PUSH_NOTIFICATION_ACTION:
+                    if (!is_numeric($actionObject['recipient']) || (intval($actionObject['recipient']) < 10000) || (intval($actionObject['recipient']) > 59999)) {
+                        return $this->Translate('Invalid ID');
+                    }
+
+                    if (!IPS_InstanceExists(intval($actionObject['recipient'])) || (IPS_GetInstance(intval($actionObject['recipient']))['ModuleInfo']['ModuleID'] != '{3565B1F2-8F7B-4311-A4B6-1BF1D868F39E}')) {
+                        return $this->Translate('No WebFront');
+                    }
+
+                    if ($actionObject['messageVariable'] !== 0) {
+                        if (!IPS_VariableExists(intval($actionObject['messageVariable']))) {
+                            return $this->Translate('No message variable');
+                        }
+
+                        if ((IPS_GetVariable(intval($actionObject['messageVariable']))['VariableType'] != 3)) {
+                            return $this->Translate('No string variable');
+                        }
+                    }
+                    break;
+            }
+
+            return $this->Translate('OK');
+        }
+
+        private function GetLevelStatus($level) {
+            $levelTable = json_decode($this->ReadPropertyString('NotificationLevels'), true);
+
+            if (($level < sizeof($levelTable)) && ($levelTable[$level - 1]['duration'] <= 0)) {
+                return $this->Translate('No duration');
+            }
+
+            foreach ($levelTable[$level - 1]['actions'] as $action) {
+                if ($this->GetActionStatus($action) != 'OK') {
+                    return $this->Translate('Faulty action');
+                }
+            }
+
+            return $this->Translate('OK');
+        }
+    }
+?>
