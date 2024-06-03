6b28cceb9a20c939f57c6de050b82da2bcede12f
Sunnyka <71286632+Sunnyka98@users.noreply.github.com>
2024-06-03 14:06:45 +0200

    if/elseif to switch

--- Notification/module.php
+++ Notification/module.php
@@ -463,11 +463,15 @@ class Notification extends WebHookModuleBenachrichtigung
                         break;
 
                     case self::PUSH_NOTIFICATION_ACTION:
-                        if(IPS_GetInstance($action['recipientObjectID'])['ModuleInfo']['ModuleID'] == '{3565B1F2-8F7B-4311-A4B6-1BF1D868F39E}') {
-                            //Only send 256 characters
-                            WFC_PushNotification($action['recipientObjectID'], $action['title'], substr($message, 0, 256), 'alarm', $this->InstanceID);
-                        }elseif( IPS_GetInstance($action['recipientObjectID'])['ModuleInfo']['ModuleID'] == '{B5B875BB-9B76-45FD-4E67-2607E45B3AC4}'){
-                            VISU_PostNotification ($action['recipientObjectID'], $action['title'], substr($message, 0, 256), 'alarm', $this->InstanceID);
+                        switch (IPS_GetInstance($action['recipientObjectID'])['ModuleInfo']['ModuleID']) {
+                            case '{3565B1F2-8F7B-4311-A4B6-1BF1D868F39E}':
+                                //Only send 256 characters
+                                WFC_PushNotification($action['recipientObjectID'], $action['title'], substr($message, 0, 256), 'alarm', $this->InstanceID);
+                                break;
+                             case '{B5B875BB-9B76-45FD-4E67-2607E45B3AC4}':
+                                //Only send 256 characters
+                                VISU_PostNotification ($action['recipientObjectID'], $action['title'], substr($message, 0, 256), 'alarm', $this->InstanceID);
+                                break;
                         }
                         break;
 
